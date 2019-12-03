688515936553fa651dcd71608af6eb13c0156bc8
Marlena Janasz <mar.janasz87@gmail.com>
2019-12-03 20:27:28 +0100

    Update app.js

--- ex4/app.js
+++ ex4/app.js
@@ -1,4 +1,5 @@
 let number;
+let opinion = [];
 for(let i =0; i<3; i++){
   number = prompt("podaj liczbe");
   if(number >7){
@@ -10,6 +11,6 @@ for(let i =0; i<3; i++){
 		else if(number<5){
 		alert('oceniłeś książkę źle!')
 		}	
-    let opinion = number;  //albo let opinion = [number];
-    console.log(opinion);
+	opinion.push(number);
 }
+console.log(opinion);
