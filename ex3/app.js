559722d5759ba0a491afd4ef232ae58ac888d55d
Marlena Janasz <mar.janasz87@gmail.com>
2020-01-07 22:54:44 +0100

    Update app.js

--- ex3/app.js
+++ ex3/app.js
@@ -1,6 +1,24 @@
-const array = ["bananab", "ananas"];
+let array = ["bananab", "ananas", "kajak", "jakaja"];
+let array2 = [];
+let output_array = [];
 
-for (let i = 0; i < array[1].length; i++ ){
-	console.log (i);
+function showArray(){
+
+	for (let i = 0; i < array.length; i++){
+
+		array2.push((array[i].split('').reverse().join('')));
+
+			if (array[i] === array2[i]){
+				//console.log('true');
+				output_array.push('true');
+			}
+			else if (array[i] !== array2[i]) {
+				//console.log('false')
+				output_array.push('false');
+				
+			}
+	}
+	console.log(output_array);
 }
 
+showArray(array);
