82c938138cf0b40984a4e243891c3507d39bf845
Marlena Janasz <mar.janasz87@gmail.com>
2021-04-12 21:20:04 +0200

    Update app.js

--- wyszukiwanie binarne/app.js
+++ wyszukiwanie binarne/app.js
@@ -1,26 +1,27 @@
 
-let doSearch = function(array, targetValue){
-	let min = 0;
-	let max = array.length-1;
-	let guess;
-		while(array.length){
-			guess = (min+max)/2;
-			if(array[guess] === targetValue){
-				return guess
-			}
-			else if(array[guess]<tagretValue){
-				return min = guess +1
-			}
-			else if(array[guess]<tagretValue){
-				return max = guess-1
-			}
-			//czy tu to powinno być? wg mnie wewnątrz pętli while:
-			else if (max < min){
-				return -1
-			}
-		
-}
+var doSearch = function(array, targetValue) {
+	var min = 0;
+	var max = array.length - 1;
+    var guess;
+    var count = 0;
+    while(max>=min){
+        guess = Math.floor((min+max)/2);
+        println(guess); 
+        count++;
+        if(array[guess] === targetValue){
+           println(count); 
+           return guess;
+         }
+        else if(array[guess] < targetValue){min = guess+1;
+         }
+        else{max = guess-1;
+         }
+    }
+	return -1;
+};
 
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-let result = doSearch(primes, 73);
+var result = doSearch(primes, 73);
+println("Found prime at index " + result);
